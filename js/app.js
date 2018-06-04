f42154deab4f6567f5a48c8e23e990d5741707f3
Krzysztof <krzyskin@wp.pl>
2018-06-05 01:39:28 +0200

    chart with simple zoom option

---
+++ js/app.js
@@ -0,0 +1,296 @@
+$(() => {
+
+    const urlApi1 = 'https://apiv2.bitcoinaverage.com/indices/global/history/BTCUSD?period=alltime&format=json';
+    const urlApi2 = 'https://apiv2.bitcoinaverage.com/indices/global/history/ETHUSD?period=alltime&format=json';
+    const urlApi3 = 'https://apiv2.bitcoinaverage.com/indices/global/history/LTCUSD?period=alltime&format=json';
+    let a = 0;
+
+
+    let btcVisible = [];
+    let ethVisible = [];
+    let ltcVisible = [];
+    let dateVisible = [];
+    let arrTimeB = [];
+    let arrAverageB = [];
+    let arrAverageE = [];
+    let arrAverageL = [];
+    let arrTimeBtc = [];
+
+    let dataFilling = function (res, newArr) {
+        console.log("działa");
+        for (let i = 0; i < res.length - 1; i++) {
+
+            let curr = res[i];
+            let next = res[i + 1];
+
+            let date = new Date(curr.time);
+            let endDate = new Date(next.time);
+
+            date = date.setDate(date.getDate() + 1);
+            endDate = endDate.setDate(endDate.getDate());
+
+            if (date === endDate) {
+                newArr.push(curr);
+            }
+
+
+            else if (date !== endDate) {
+
+                function fillDates(start, end) {
+                    let output = [];
+
+                    do {
+                        output.push({
+                            "time": start.toISOString().substring(0, 10),
+                            "average": undefined
+                        });
+                        start.setDate(start.getDate() + 1);
+
+                    } while (start <= end);
+
+                    for (let i = 0; i < output.length; i++) {
+                        newArr.push(output[i]);
+                    }
+                }
+
+                let start = new Date(date);
+                let end = new Date(endDate);
+                fillDates(start, end);
+            }
+
+        }
+        newArr.push(res[res.length - 1]);
+
+    };
+
+    $.ajax({
+        url: urlApi1
+    }).done(function (res) {
+
+        res = res.reverse();
+        let newArr = [];
+        for (let i = 0; i < res.length; i++) {
+            arrTimeBtc.push(res[i].time);
+        }
+        let index = arrTimeBtc.indexOf("2016-03-07 00:00:00");
+        res.splice(0, index);
+
+        dataFilling(res, newArr);
+
+        for (let i = 0; i < newArr.length; i++) {
+            arrTimeB.push(newArr[i].time.substring(0, 10));
+            arrAverageB.push(newArr[i].average);
+        }
+
+    }).fail(function () {
+        console.log('fail');
+    });
+    $.ajax({
+        url: urlApi2
+    }).done(function (res) {
+        res = res.reverse();
+
+        let newArr = [];
+        dataFilling(res, newArr);
+
+        for (let i = 0; i < newArr.length; i++) {
+            arrAverageE.push(newArr[i].average);
+        }
+    }).fail(function () {
+        console.log('fail');
+    });
+    $.ajax({
+        url: urlApi3
+    }).done(function (res) {
+
+        res = res.reverse();
+        let newArr = [];
+        dataFilling(res, newArr);
+        for (let i = 0; i < newArr.length; i++) {
+            arrAverageL.push(newArr[i].average);
+        }
+
+    }).fail(function () {
+        console.log('fail');
+    });
+
+    btcVisible = arrAverageB;
+    ethVisible = arrAverageE;
+    ltcVisible = arrAverageL;
+    dateVisible = arrTimeB;
+
+    let data = {
+        labels: dateVisible,
+        datasets: [{
+            label: "My First dataset",
+            //linia
+            //borderDash: [3, 3], //jezeli ustawione to przerywana linia
+            borderColor: 'rgba(236,115,87, 0.7)',
+            pointBorderColor: 'rgba(236,115,87, 0.7)',
+            borderWidth: 2,
+            //kolor tla i legendy
+            fill: true, //czy wypelnic zbior
+            backgroundColor: 'rgba(236,115,87, 0.1)', //wplywa tez na kolor w legendzie
+            //ustawienia punktu
+            pointRadius: 4,
+            pointBorderWidth: 1,
+            pointBackgroundColor: 'rgba(255,255,255,1)',
+            //ustawienia punktu hover
+            pointHoverRadius: 4,
+            pointHoverBorderWidth: 3,
+            pointHoverBackgroundColor: 'rgba(255,255,255,1)',
+            pointHoverBorderColor: 'rgba(236,115,87, 1)',
+            data: btcVisible,
+        },
+            {
+                label: "My Second dataset",
+                borderColor: 'rgba(75,192,192, 0.7)',
+                pointBorderColor: 'rgba(75,115,87, 0.7)',
+                borderWidth: 2,
+                //kolor tla i legendy
+                fill: true, //czy wypelnic zbior
+                backgroundColor: 'rgba(236,115,87, 0.1)', //wplywa tez na kolor w legendzie
+                //ustawienia punktu
+                pointRadius: 4,
+                pointBorderWidth: 1,
+                pointBackgroundColor: 'rgba(255,255,255,1)',
+                //ustawienia punktu hover
+                pointHoverRadius: 4,
+                pointHoverBorderWidth: 3,
+                pointHoverBackgroundColor: 'rgba(255,255,255,1)',
+                pointHoverBorderColor: 'rgba(236,115,87, 1)',
+
+                data: ethVisible,
+            },
+            {
+                label: "My Third dataset",
+                borderColor: 'rgba(132,177,237, 0.7)',
+                pointBorderColor: 'rgba(132,115,87, 0.7)',
+                borderWidth: 2,
+                //kolor tla i legendy
+                fill: true, //czy wypelnic zbior
+                backgroundColor: 'rgba(236,115,87, 0.1)', //wplywa tez na kolor w legendzie
+                //ustawienia punktu
+                pointRadius: 4,
+                pointBorderWidth: 1,
+                pointBackgroundColor: 'rgba(255,255,255,1)',
+                //ustawienia punktu hover
+                pointHoverRadius: 4,
+                pointHoverBorderWidth: 3,
+                pointHoverBackgroundColor: 'rgba(255,255,255,1)',
+                pointHoverBorderColor: 'rgba(236,115,87, 1)',
+
+                data: ltcVisible,
+            }
+        ]
+    };
+
+    let options = {
+        scales: {
+            xAxes: [{ //linie x
+                gridLines: {
+                    zeroLineWidth: 1, //linia x=0
+                    zeroLineColor: 'rgba(0,0,0,0.3)', //kolor lini x=0
+                    color: "rgba(0, 0, 0, 0.05)", //kolor linii
+                    lineWidth: 1 //szerokośc linii
+                },
+                display: true, //czy pokazywac dolne opisy jednostek
+                scaleLabel: { //tytuł osi x
+                    display: true,
+                    labelString: 'Date',
+                    fontSize: 12,
+                    fontStyle: 'bold'
+                },
+                ticks: { //rozmiar jednostek
+                    fontSize: 10
+                }
+            }],
+            yAxes: [{
+                type: 'logarithmic',
+                ticks: {
+                    min: 0,
+                    max: 20000
+                }
+            }]
+        }
+    };
+    console.log(arrAverageL,arrTimeB,arrAverageE,arrAverageL);
+
+
+    function updateChart(chart) {
+        chart.data.labels = dateVisible;
+        chart.data.datasets[0].data = btcVisible;
+        chart.data.datasets[1].data = ethVisible;
+        chart.data.datasets[2].data = ltcVisible;
+        chart.update();
+    }
+    function setRange (a, z) {
+        btcVisible = arrAverageB.slice(a, z);
+        ethVisible = arrAverageE.slice(a, z);
+        ltcVisible = arrAverageL.slice(a, z);
+        dateVisible = arrTimeB.slice(a, z);
+        return (btcVisible, ethVisible, ltcVisible, dateVisible);
+    }
+    $('.start').on('click', function () {
+
+        let ctx = document.getElementById("myChart").getContext("2d");
+
+        let myLineChart = new Chart(ctx, {
+            type: 'line',
+            data: data,
+            options: options
+        });
+
+
+
+
+        let z = arrTimeB.length;
+        $('.plus').on('click', function () {
+
+            if (z-a>=100) {
+                a = a + 50;
+                z = z - 50;
+            }
+            setRange(a, z);
+            console.log(a, z);
+            updateChart(myLineChart);
+
+        })
+        $('.minus').on('click', function () {
+
+
+            if (a >= 50) {
+                a = a - 50
+            }
+            if (z <= (arrTimeB.length - 50)) {
+                z = z + 50
+            }
+
+            //let z = arrTimeB.length-count*50;
+            console.log(a, z);
+            setRange(a, z);
+            updateChart(myLineChart);
+        });
+        $('.right').on('click', function () {
+
+            if(z<=arrTimeB.length-50){
+                a=a+50;
+                z=z+50
+            }
+            console.log(a, z);
+            setRange(a, z);
+            updateChart(myLineChart);
+        })
+        $('.left').on('click', function () {
+
+            if(a>=50){
+                a=a-50;
+                z=z-50;
+            }
+            console.log(a, z);
+            setRange(a, z);
+            updateChart(myLineChart);
+        })
+    })
+
+})
--- js/out.js
+++ js/out.js
@@ -1,1 +1,112 @@
-!function(e){var o={};function t(r){if(o[r])return o[r].exports;var n=o[r]={i:r,l:!1,exports:{}};return e[r].call(n.exports,n,n.exports,t),n.l=!0,n.exports}t.m=e,t.c=o,t.d=function(e,o,r){t.o(e,o)||Object.defineProperty(e,o,{enumerable:!0,get:r})},t.r=function(e){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},t.t=function(e,o){if(1&o&&(e=t(e)),8&o)return e;if(4&o&&"object"==typeof e&&e&&e.__esModule)return e;var r=Object.create(null);if(t.r(r),Object.defineProperty(r,"default",{enumerable:!0,value:e}),2&o&&"string"!=typeof e)for(var n in e)t.d(r,n,function(o){return e[o]}.bind(null,n));return r},t.n=function(e){var o=e&&e.__esModule?function(){return e.default}:function(){return e};return t.d(o,"a",o),o},t.o=function(e,o){return Object.prototype.hasOwnProperty.call(e,o)},t.p="",t(t.s=0)}([function(e,o){$(()=>{let e=0,o=[],t=[],r=[],n=[],a=[],i=[],l=[],d=[],s=[],c=function(e,o){console.log("działa");for(let r=0;r<e.length-1;r++){let n=e[r],a=e[r+1],i=new Date(n.time),l=new Date(a.time);if((i=i.setDate(i.getDate()+1))===(l=l.setDate(l.getDate())))o.push(n);else if(i!==l){function t(e,t){let r=[];do{r.push({time:e.toISOString().substring(0,10),average:void 0}),e.setDate(e.getDate()+1)}while(e<=t);for(let e=0;e<r.length;e++)o.push(r[e])}t(new Date(i),new Date(l))}}o.push(e[e.length-1])};$.ajax({url:"https://apiv2.bitcoinaverage.com/indices/global/history/BTCUSD?period=alltime&format=json"}).done(function(e){e=e.reverse();let o=[];for(let o=0;o<e.length;o++)s.push(e[o].time);let t=s.indexOf("2016-03-07 00:00:00");e.splice(0,t),c(e,o);for(let e=0;e<o.length;e++)a.push(o[e].time.substring(0,10)),i.push(o[e].average)}).fail(function(){console.log("fail")}),$.ajax({url:"https://apiv2.bitcoinaverage.com/indices/global/history/ETHUSD?period=alltime&format=json"}).done(function(e){e=e.reverse();let o=[];c(e,o);for(let e=0;e<o.length;e++)l.push(o[e].average)}).fail(function(){console.log("fail")}),$.ajax({url:"https://apiv2.bitcoinaverage.com/indices/global/history/LTCUSD?period=alltime&format=json"}).done(function(e){e=e.reverse();let o=[];c(e,o);for(let e=0;e<o.length;e++)d.push(o[e].average)}).fail(function(){console.log("fail")});let u={labels:n=a,datasets:[{label:"My First dataset",borderColor:"rgba(236,115,87, 0.7)",pointBorderColor:"rgba(236,115,87, 0.7)",borderWidth:2,fill:!0,backgroundColor:"rgba(236,115,87, 0.1)",pointRadius:4,pointBorderWidth:1,pointBackgroundColor:"rgba(255,255,255,1)",pointHoverRadius:4,pointHoverBorderWidth:3,pointHoverBackgroundColor:"rgba(255,255,255,1)",pointHoverBorderColor:"rgba(236,115,87, 1)",data:o=i},{label:"My Second dataset",borderColor:"rgba(75,192,192, 0.7)",pointBorderColor:"rgba(75,115,87, 0.7)",borderWidth:2,fill:!0,backgroundColor:"rgba(236,115,87, 0.1)",pointRadius:4,pointBorderWidth:1,pointBackgroundColor:"rgba(255,255,255,1)",pointHoverRadius:4,pointHoverBorderWidth:3,pointHoverBackgroundColor:"rgba(255,255,255,1)",pointHoverBorderColor:"rgba(236,115,87, 1)",data:t=l},{label:"My Third dataset",borderColor:"rgba(132,177,237, 0.7)",pointBorderColor:"rgba(132,115,87, 0.7)",borderWidth:2,fill:!0,backgroundColor:"rgba(236,115,87, 0.1)",pointRadius:4,pointBorderWidth:1,pointBackgroundColor:"rgba(255,255,255,1)",pointHoverRadius:4,pointHoverBorderWidth:3,pointHoverBackgroundColor:"rgba(255,255,255,1)",pointHoverBorderColor:"rgba(236,115,87, 1)",data:r=d}]},g={scales:{xAxes:[{gridLines:{zeroLineWidth:1,zeroLineColor:"rgba(0,0,0,0.3)",color:"rgba(0, 0, 0, 0.05)",lineWidth:1},display:!0,scaleLabel:{display:!0,labelString:"Date",fontSize:12,fontStyle:"bold"},ticks:{fontSize:10}}],yAxes:[{type:"logarithmic",ticks:{min:0,max:2e4}}]}};function f(e){e.data.labels=n,e.data.datasets[0].data=o,e.data.datasets[1].data=t,e.data.datasets[2].data=r,e.update()}console.log(d,a,l,d),$(".start").on("click",function(){let s=document.getElementById("myChart").getContext("2d"),c=new Chart(s,{type:"line",data:u,options:g});setRange=function(e,s){return o=i.slice(e,s),t=l.slice(e,s),r=d.slice(e,s),n=a.slice(e,s)};let p=a.length;$(".plus").on("click",function(){p-e>=100&&(e+=50,p-=50),setRange(e,p),console.log(e,p),f(c)}),$(".minus").on("click",function(){e>=50&&(e-=50),p<=a.length-50&&(p+=50),console.log(e,p),setRange(e,p),f(c)}),$(".right").on("click",function(){p<=a.length-50&&(e+=50,p+=50),console.log(e,p),setRange(e,p),f(c)}),$(".left").on("click",function(){e>=50&&(e-=50,p-=50),console.log(e,p),setRange(e,p),f(c)})})})}]);
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./js/app.js":
+/*!*******************!*\
+  !*** ./js/app.js ***!
+  \*******************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+eval("\n\n$(function () {\n\n    var urlApi1 = 'https://apiv2.bitcoinaverage.com/indices/global/history/BTCUSD?period=alltime&format=json';\n    var urlApi2 = 'https://apiv2.bitcoinaverage.com/indices/global/history/ETHUSD?period=alltime&format=json';\n    var urlApi3 = 'https://apiv2.bitcoinaverage.com/indices/global/history/LTCUSD?period=alltime&format=json';\n    var a = 0;\n\n    var btcVisible = [];\n    var ethVisible = [];\n    var ltcVisible = [];\n    var dateVisible = [];\n    var arrTimeB = [];\n    var arrAverageB = [];\n    var arrAverageE = [];\n    var arrAverageL = [];\n    var arrTimeBtc = [];\n\n    var dataFilling = function dataFilling(res, newArr) {\n        console.log(\"działa\");\n        for (var i = 0; i < res.length - 1; i++) {\n\n            var curr = res[i];\n            var next = res[i + 1];\n\n            var date = new Date(curr.time);\n            var endDate = new Date(next.time);\n\n            date = date.setDate(date.getDate() + 1);\n            endDate = endDate.setDate(endDate.getDate());\n\n            if (date === endDate) {\n                newArr.push(curr);\n            } else if (date !== endDate) {\n                var fillDates = function fillDates(start, end) {\n                    var output = [];\n\n                    do {\n                        output.push({\n                            \"time\": start.toISOString().substring(0, 10),\n                            \"average\": undefined\n                        });\n                        start.setDate(start.getDate() + 1);\n                    } while (start <= end);\n\n                    for (var _i = 0; _i < output.length; _i++) {\n                        newArr.push(output[_i]);\n                    }\n                };\n\n                var start = new Date(date);\n                var end = new Date(endDate);\n                fillDates(start, end);\n            }\n        }\n        newArr.push(res[res.length - 1]);\n    };\n\n    $.ajax({\n        url: urlApi1\n    }).done(function (res) {\n\n        res = res.reverse();\n        var newArr = [];\n        for (var i = 0; i < res.length; i++) {\n            arrTimeBtc.push(res[i].time);\n        }\n        var index = arrTimeBtc.indexOf(\"2016-03-07 00:00:00\");\n        res.splice(0, index);\n\n        dataFilling(res, newArr);\n\n        for (var _i2 = 0; _i2 < newArr.length; _i2++) {\n            arrTimeB.push(newArr[_i2].time.substring(0, 10));\n            arrAverageB.push(newArr[_i2].average);\n        }\n    }).fail(function () {\n        console.log('fail');\n    });\n    $.ajax({\n        url: urlApi2\n    }).done(function (res) {\n        res = res.reverse();\n\n        var newArr = [];\n        dataFilling(res, newArr);\n\n        for (var i = 0; i < newArr.length; i++) {\n            arrAverageE.push(newArr[i].average);\n        }\n    }).fail(function () {\n        console.log('fail');\n    });\n    $.ajax({\n        url: urlApi3\n    }).done(function (res) {\n\n        res = res.reverse();\n        var newArr = [];\n        dataFilling(res, newArr);\n        for (var i = 0; i < newArr.length; i++) {\n            arrAverageL.push(newArr[i].average);\n        }\n    }).fail(function () {\n        console.log('fail');\n    });\n\n    btcVisible = arrAverageB;\n    ethVisible = arrAverageE;\n    ltcVisible = arrAverageL;\n    dateVisible = arrTimeB;\n\n    var data = {\n        labels: dateVisible,\n        datasets: [{\n            label: \"My First dataset\",\n            //linia\n            //borderDash: [3, 3], //jezeli ustawione to przerywana linia\n            borderColor: 'rgba(236,115,87, 0.7)',\n            pointBorderColor: 'rgba(236,115,87, 0.7)',\n            borderWidth: 2,\n            //kolor tla i legendy\n            fill: true, //czy wypelnic zbior\n            backgroundColor: 'rgba(236,115,87, 0.1)', //wplywa tez na kolor w legendzie\n            //ustawienia punktu\n            pointRadius: 4,\n            pointBorderWidth: 1,\n            pointBackgroundColor: 'rgba(255,255,255,1)',\n            //ustawienia punktu hover\n            pointHoverRadius: 4,\n            pointHoverBorderWidth: 3,\n            pointHoverBackgroundColor: 'rgba(255,255,255,1)',\n            pointHoverBorderColor: 'rgba(236,115,87, 1)',\n            data: btcVisible\n        }, {\n            label: \"My Second dataset\",\n            borderColor: 'rgba(75,192,192, 0.7)',\n            pointBorderColor: 'rgba(75,115,87, 0.7)',\n            borderWidth: 2,\n            //kolor tla i legendy\n            fill: true, //czy wypelnic zbior\n            backgroundColor: 'rgba(236,115,87, 0.1)', //wplywa tez na kolor w legendzie\n            //ustawienia punktu\n            pointRadius: 4,\n            pointBorderWidth: 1,\n            pointBackgroundColor: 'rgba(255,255,255,1)',\n            //ustawienia punktu hover\n            pointHoverRadius: 4,\n            pointHoverBorderWidth: 3,\n            pointHoverBackgroundColor: 'rgba(255,255,255,1)',\n            pointHoverBorderColor: 'rgba(236,115,87, 1)',\n\n            data: ethVisible\n        }, {\n            label: \"My Third dataset\",\n            borderColor: 'rgba(132,177,237, 0.7)',\n            pointBorderColor: 'rgba(132,115,87, 0.7)',\n            borderWidth: 2,\n            //kolor tla i legendy\n            fill: true, //czy wypelnic zbior\n            backgroundColor: 'rgba(236,115,87, 0.1)', //wplywa tez na kolor w legendzie\n            //ustawienia punktu\n            pointRadius: 4,\n            pointBorderWidth: 1,\n            pointBackgroundColor: 'rgba(255,255,255,1)',\n            //ustawienia punktu hover\n            pointHoverRadius: 4,\n            pointHoverBorderWidth: 3,\n            pointHoverBackgroundColor: 'rgba(255,255,255,1)',\n            pointHoverBorderColor: 'rgba(236,115,87, 1)',\n\n            data: ltcVisible\n        }]\n    };\n\n    var options = {\n        scales: {\n            xAxes: [{ //linie x\n                gridLines: {\n                    zeroLineWidth: 1, //linia x=0\n                    zeroLineColor: 'rgba(0,0,0,0.3)', //kolor lini x=0\n                    color: \"rgba(0, 0, 0, 0.05)\", //kolor linii\n                    lineWidth: 1 //szerokośc linii\n                },\n                display: true, //czy pokazywac dolne opisy jednostek\n                scaleLabel: { //tytuł osi x\n                    display: true,\n                    labelString: 'Date',\n                    fontSize: 12,\n                    fontStyle: 'bold'\n                },\n                ticks: { //rozmiar jednostek\n                    fontSize: 10\n                }\n            }],\n            yAxes: [{\n                type: 'logarithmic',\n                ticks: {\n                    min: 0,\n                    max: 20000\n                }\n            }]\n        }\n    };\n    console.log(arrAverageL, arrTimeB, arrAverageE, arrAverageL);\n\n    function updateChart(chart) {\n        chart.data.labels = dateVisible;\n        chart.data.datasets[0].data = btcVisible;\n        chart.data.datasets[1].data = ethVisible;\n        chart.data.datasets[2].data = ltcVisible;\n        chart.update();\n    }\n    function setRange(a, z) {\n        btcVisible = arrAverageB.slice(a, z);\n        ethVisible = arrAverageE.slice(a, z);\n        ltcVisible = arrAverageL.slice(a, z);\n        dateVisible = arrTimeB.slice(a, z);\n        return btcVisible, ethVisible, ltcVisible, dateVisible;\n    }\n    $('.start').on('click', function () {\n\n        var ctx = document.getElementById(\"myChart\").getContext(\"2d\");\n\n        var myLineChart = new Chart(ctx, {\n            type: 'line',\n            data: data,\n            options: options\n        });\n\n        var z = arrTimeB.length;\n        $('.plus').on('click', function () {\n\n            if (z - a >= 100) {\n                a = a + 50;\n                z = z - 50;\n            }\n            setRange(a, z);\n            console.log(a, z);\n            updateChart(myLineChart);\n        });\n        $('.minus').on('click', function () {\n\n            if (a >= 50) {\n                a = a - 50;\n            }\n            if (z <= arrTimeB.length - 50) {\n                z = z + 50;\n            }\n\n            //let z = arrTimeB.length-count*50;\n            console.log(a, z);\n            setRange(a, z);\n            updateChart(myLineChart);\n        });\n        $('.right').on('click', function () {\n\n            if (z <= arrTimeB.length - 50) {\n                a = a + 50;\n                z = z + 50;\n            }\n            console.log(a, z);\n            setRange(a, z);\n            updateChart(myLineChart);\n        });\n        $('.left').on('click', function () {\n\n            if (a >= 50) {\n                a = a - 50;\n                z = z - 50;\n            }\n            console.log(a, z);\n            setRange(a, z);\n            updateChart(myLineChart);\n        });\n    });\n});\n\n//# sourceURL=webpack:///./js/app.js?");
+
+/***/ }),
+
+/***/ 0:
+/*!*************************!*\
+  !*** multi ./js/app.js ***!
+  \*************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+eval("module.exports = __webpack_require__(/*! /home/krzysztof/Pulpit/repozytoria/cryptocurrencies-comparison/js/app.js */\"./js/app.js\");\n\n\n//# sourceURL=webpack:///multi_./js/app.js?");
+
+/***/ })
+
+/******/ });
